21b0879f74793a06b5b624ef4524fd28bd6b8665
akira-toriyama <92862731+akira-toriyama@users.noreply.github.com>
2022-09-25 21:15:59 +0900

    xxx: :art:
    x

--- src/externalInterface/git.ts
+++ src/externalInterface/git.ts
@@ -1,11 +1,9 @@
-// import { join } from "https://deno.land/std/path/mod.ts";
+import { join } from "https://deno.land/std/path/mod.ts";
 
+const commitEditmsgPath = join(".git", "COMMIT_EDITMSG");
 type SetCommitMessage = (p: { message: string }) => Promise<void>;
 /**
  * Set a Message for `./.git/COMMIT_EDITMSG`.
  */
 export const setCommitMessage: SetCommitMessage = (p) =>
-  Deno.writeTextFile(
-    ".git/COMMIT_EDITMSG",
-    p.message,
-  ).catch(console.error);
+  Deno.writeTextFile(commitEditmsgPath, p.message).catch(console.error);
