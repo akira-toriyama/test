d5c1bceaa3794e6dd493fe228cd7ea16ca7a0a5f
akira-toriyama <92862731+akira-toriyama@users.noreply.github.com>
2022-09-27 21:04:20 +0900

    :speech_balloon:(x.x.o): Fix separator

--- src/useCase/gitmojiCommits/run.ts
+++ src/useCase/gitmojiCommits/run.ts
@@ -17,7 +17,7 @@ const issues = await gitHub.fetchIssues()
   ).then((r) => {
     return [
       { name: "Not selected", value: "Not selected" },
-      { name: "-------------", value: "", disabled: true },
+      { name: "👇🏻👇🏻👇🏻👇🏻👇🏻👇🏻👇🏻", value: "", disabled: true },
       ...r,
     ];
   })
