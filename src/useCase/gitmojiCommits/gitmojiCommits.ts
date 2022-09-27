daba5ec9309ba875088b13a745b74496bdf96618
akira-toriyama <92862731+akira-toriyama@users.noreply.github.com>
2022-09-27 19:31:17 +0900

    :mute: This is a pen

--- src/useCase/gitmojiCommits/gitmojiCommits.ts
+++ src/useCase/gitmojiCommits/gitmojiCommits.ts
@@ -5,6 +5,7 @@ import {
   SelectOptions,
 } from "https://deno.land/x/cliffy@v0.25.0/prompt/mod.ts";
 import * as gitHub from "../../externalInterface/gitHub.ts";
+import * as gitmoji from "../../externalInterface/gitmoji.ts";
 import * as templateService from "../../service/template.ts";
 import * as terminal from "../../userInterface/terminal.ts";
 import { colors } from "https://deno.land/x/cliffy@v0.25.0/ansi/colors.ts";
@@ -91,43 +92,75 @@ export const main: Main = async (p) => {
     p,
   );
 
+  const validate = async (pp: {
+    input: string;
+    name: "subject" | "body";
+  }) => {
+    if (p.question[pp.name]?.validate === undefined) {
+      return true;
+    }
+
+    terminal.spinner.start({ text: "Submitting..." });
+    const r = await p.question[pp.name]?.validate(pp.input)
+      .finally(() => terminal.spinner.stop());
+
+    if (r === undefined) {
+      return true;
+    }
+
+    if (r.type === "valid") {
+      return true;
+    }
+
+    state.template = templateService.templateFillIn({
+      template: state.template,
+      answerVo: {},
+    });
+
+    templateRender({
+      value: prepareTemplate({
+        template: state.template,
+        name: pp.name,
+      }),
+    });
+
+    console.error(JSON.stringify(r.reason, null, 2));
+
+    // error;
+    return "";
+  };
+
+  const beforeFn = (pp: { answerVo: Record<string, string | undefined> }) => {
+    state.template = templateService.templateFillIn({
+      template: state.template,
+      answerVo: pp.answerVo,
+    });
+
+    templateRender({
+      value: prepareTemplate({
+        template: state.template,
+        name: "subject",
+      }),
+    });
+  };
+
+  const afterFn = (pp: { answerVo: Record<string, string | undefined> }) => {
+    state.template = templateService.templateFillIn({
+      template: state.template,
+      answerVo: pp.answerVo,
+    });
+  };
+
   return prompt([
     {
-      name: "subject",
-      message: "Enter subject.",
-      type: Input,
-      hint:
-        "Surrounding it with an ` allows it. example: Add myFunc -> Add `myFunc`",
-      validate: async (input) => {
-        if (p.question.subject?.validate === undefined) {
-          return true;
-        }
-
-        terminal.spinner.start({ text: "Submitting..." });
-        const r = await p.question.subject?.validate(input)
-          .finally(() => terminal.spinner.stop());
-
-        if (r.type === "valid") {
-          return true;
-        }
-
-        state.template = templateService.templateFillIn({
-          template: state.template,
-          answerVo: {},
-        });
-
-        templateRender({
-          value: prepareTemplate({
-            template: state.template,
-            name: "subject",
-          }),
-        });
-
-        console.error(JSON.stringify(r.reason, null, 2));
-
-        // error;
-        return "";
-      },
+      name: "gitmoji",
+      message: "Select a gitmoji.",
+      type: Select,
+      options: gitmoji.getGitmojis().map((v) => ({
+        name: `${v.emoji}: ${v.description}`,
+        value: v.code,
+      })),
+      search: true,
       before: async (answerVo, next) => {
         state.template = templateService.templateFillIn({
           template: state.template,
@@ -137,7 +170,7 @@ export const main: Main = async (p) => {
         templateRender({
           value: prepareTemplate({
             template: state.template,
-            name: "subject",
+            name: "gitmoji",
           }),
         });
 
@@ -148,7 +181,22 @@ export const main: Main = async (p) => {
           template: state.template,
           answerVo,
         });
-
+        await next();
+      },
+    },
+    {
+      name: "subject",
+      message: "Enter subject.",
+      type: Input,
+      hint:
+        "Surrounding it with an ` allows it. example: Add myFunc -> Add `myFunc`",
+      validate: (input) => validate({ input, name: "subject" }),
+      before: async (answerVo, next) => {
+        beforeFn({ answerVo });
+        await next();
+      },
+      after: async (answerVo, next) => {
+        afterFn({ answerVo });
         await next();
       },
     },
@@ -158,57 +206,13 @@ export const main: Main = async (p) => {
       type: Input,
       hint:
         "Surrounding it with an ` allows it. example: Add myFunc -> Add `myFunc`",
-      validate: async (input) => {
-        if (p.question.body?.validate === undefined) {
-          return true;
-        }
-
-        terminal.spinner.start({ text: "Submitting..." });
-        const r = await p.question.body?.validate(input)
-          .finally(() => terminal.spinner.stop());
-
-        if (r.type === "valid") {
-          return true;
-        }
-
-        state.template = templateService.templateFillIn({
-          template: state.template,
-          answerVo: {},
-        });
-
-        templateRender({
-          value: prepareTemplate({
-            template: state.template,
-            name: "body",
-          }),
-        });
-
-        console.error(JSON.stringify(r.reason, null, 2));
-
-        // error;
-        return "";
-      },
+      validate: (input) => validate({ input, name: "body" }),
       before: async (answerVo, next) => {
-        state.template = templateService.templateFillIn({
-          template: state.template,
-          answerVo,
-        });
-
-        templateRender({
-          value: prepareTemplate({
-            template: state.template,
-            name: "body",
-          }),
-        });
-
+        beforeFn({ answerVo });
         await next();
       },
       after: async (answerVo, next) => {
-        state.template = templateService.templateFillIn({
-          template: state.template,
-          answerVo,
-        });
-
+        afterFn({ answerVo });
         state.fixTemplateBody();
         await next();
       },
