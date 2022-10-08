9de92acce155e1b963c73287b5502ea1d0090422
akira-toriyama <92862731+akira-toriyama@users.noreply.github.com>
2022-10-08 15:33:22 +0900

    :construction:[x.x.x]: W.I.P

--- a.ts
+++ a.ts
@@ -1,21 +1,18 @@
-// import {
-//   Command,
-//   EnumType,
-// } from "https://deno.land/x/cliffy@v0.25.0/command/mod.ts";
+import {
+  Command,
+  EnumType,
+} from "https://deno.land/x/cliffy@v0.25.0/command/mod.ts";
 
-// await new Command()
-//   .name("cliffy")
-//   .version("0.1.0")
-//   .description("Command line framework for Deno")
-//   .type("log-level", new EnumType(["debug", "info", "warn", "error"]))
-//   .env("DEBUG=<enable:boolean>", "Enable debug output.")
-//   .option("-d, --debug", "Enable debug output.")
-//   .option("-l, --log-level <level:log-level>", "Set log level.", {
-//     default: "info" as const,
-//   })
-//   .arguments("<input:string> [output:string]")
-//   .action((options, ...args) => {})
-//   .parse(Deno.args);
-
-// ok
-// deno run --import-map="https://deno.land/x/at_test@v1.3.4/import_map.json?source"  --unstable --allow-net --allow-env --allow-run --allow-write "https://deno.land/x/at_test@v1.3.4/src/useCase/gitmojiCommits/run.ts"
+await new Command()
+  .name("cliffy")
+  .version("0.1.0")
+  .description("Command line framework for Deno")
+  .type("log-level", new EnumType(["debug", "info", "warn", "error"]))
+  .env("DEBUG=<enable:boolean>", "Enable debug output.")
+  .option("-d, --debug", "Enable debug output.")
+  .option("-l, --log-level <level:log-level>", "Set log level.", {
+    default: "info" as const,
+  })
+  .arguments("<input:string> [output:string]")
+  .action((options, ...args) => {})
+  .parse(Deno.args);
